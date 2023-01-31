c033c7c9559a1fe9995b78535a556975949b9fd5
Thiago Holder <thiago.holder@farfetch.com>
2023-01-31 13:57:56 +0000

    Add transaction pool in p2p-server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,10 +1,15 @@
 const Websocket = require("ws");
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
+const MESSAGE_TYPES = {
+  chain: "CHAIN",
+  transaction: "TRANSACTION",
+};
 
 class P2pServer {
-  constructor(blockchain) {
+  constructor(blockchain, transactionPool) {
     this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
     this.socket = [];
   }
 
@@ -33,18 +38,40 @@ class P2pServer {
   }
 
   sendChain(socket) {
-    socket.send(JSON.stringify(this.blockchain.chain));
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.chain,
+        chain: this.blockchain.chain,
+      })
+    );
+  }
+
+  sendTransaction(socket, transaction) {
+    socket.send(
+      JSON.stringify({ type: MESSAGE_TYPES.transaction, transaction })
+    );
   }
 
   messageHandler(socket) {
     socket.on("message", (message) => {
       const data = JSON.parse(message);
-      this.blockchain.replaceChain(data);
+      switch (data.type) {
+        case MESSAGE_TYPES.chain:
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case MESSAGE_TYPES.transaction:
+          this.transactionPool.updateOrAddTransaction(data.transaction);
+          break;
+      }
     });
   }
 
   syncChain() {
     this.socket.forEach((socket) => this.sendChain(socket));
+  }
+
+  broadcastTransaction(transaction) {
+    this.socket.forEach((socket) => this.sendTransaction(socket, transaction));
   }
 }
 
